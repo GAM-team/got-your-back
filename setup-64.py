0984d6e2648484561d8bffb728ebf961778ca346
Jay Lee <jay0lee@gmail.com>
2014-06-17 14:37:16 -0400

    Add files for building Windows releases to git

---
+++ setup-64.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+import py2exe, sys, os
+
+sys.argv.append('py2exe')
+
+setup(
+  console = ['gyb.py'],
+
+  zipfile = None,
+  options = {'py2exe': 
+              {'optimize': 2,
+               'bundle_files': 3,
+               'dist_dir' : 'gyb-64',
+               'compressed' : True}
+            }
+  )
