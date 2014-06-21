1acc7b725cfb6e440cf613b6f315c25a4938ddd8
Jay Lee <jay0lee@gmail.com>
2014-06-21 14:38:08 -0400

    use utf-8 by default

--- gyb.py
+++ gyb.py
@@ -129,6 +129,26 @@ def SetupOptionParser():
     help='Display this message.')
   return parser
 
+def win32_unicode_argv():
+  from ctypes import POINTER, byref, cdll, c_int, windll
+  from ctypes.wintypes import LPCWSTR, LPWSTR
+
+  GetCommandLineW = cdll.kernel32.GetCommandLineW
+  GetCommandLineW.argtypes = []
+  GetCommandLineW.restype = LPCWSTR
+
+  CommandLineToArgvW = windll.shell32.CommandLineToArgvW
+  CommandLineToArgvW.argtypes = [LPCWSTR, POINTER(c_int)]
+  CommandLineToArgvW.restype = POINTER(LPWSTR)
+
+  cmd = GetCommandLineW()
+  argc = c_int(0)
+  argv = CommandLineToArgvW(cmd, byref(argc))
+  if argc.value > 0:
+    # Remove Python executable and commands if present
+    start = argc.value - len(sys.argv)
+    return [argv[i] for i in xrange(start, argc.value)]
+
 def getProgPath():
   if os.path.abspath('/') != -1:
     divider = '/'
@@ -1200,6 +1220,10 @@ def main(argv):
     pass
   
 if __name__ == '__main__':
+  reload(sys)
+  sys.setdefaultencoding(u'UTF-8')
+  if os.name == u'nt':
+    sys.argv = win32_unicode_argv() # cleanup sys.argv on Windows
   try:
     main(sys.argv[1:])
   except KeyboardInterrupt:
