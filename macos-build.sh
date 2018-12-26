5d81bb0f4b6bbb74f7d196f865f84d0613c03540
Jay Lee <jay0lee@gmail.com>
2018-12-26 14:56:06 -0500

    Use Python 3.7 on Mac

--- macos-build.sh
+++ macos-build.sh
@@ -3,6 +3,6 @@ rm -rf build
 rm -rf dist
 rm -rf gyb-$1-macos.tar.xz
 
-/Library/Frameworks/Python.framework/Versions/3.6/bin/pyinstaller -F --clean --distpath=gyb macos-gyb.spec
+/Library/Frameworks/Python.framework/Versions/3.7/bin/pyinstaller -F --clean --distpath=gyb macos-gyb.spec
 cp LICENSE gyb/
 tar cJf gyb-$1-macos.tar.xz gyb/
