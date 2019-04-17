da0456250c606a02a9ddc1b025582671c0990bdd
Jay Lee <jay0lee@gmail.com>
2019-04-16 20:14:12 -0400

    Update windows-x86_64-before-install.sh

--- travis/windows-x86_64-before-install.sh
+++ travis/windows-x86_64-before-install.sh
@@ -1,4 +1,4 @@
-powershell Install-WindowsFeature Net-Framework-Core
 cinst $CINST_ARGS python3
+powershell Install-WindowsFeature Net-Framework-Core
 export PATH=$PATH:/c/Python37/scripts
 cinst -y wixtoolset
