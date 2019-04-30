1b4fe9c74bee307070ae2dbb91103071135cea43
Jay Lee <jay0lee@gmail.com>
2019-04-30 11:18:16 -0400

    Update windows-x86-before-install.sh

--- travis/windows-x86-before-install.sh
+++ travis/windows-x86-before-install.sh
@@ -4,3 +4,6 @@ cinst -y $CINST_ARGS openssl.light
 cp -v /c/Program\ Files/OpenSSL/bin/*.dll /c/Python37/DLLs
 export PATH=$PATH:/c/Python37/scripts
 cinst -y wixtoolset
+
+export python=/c/Python37/python
+export pip=/c/Python37/script/pip
