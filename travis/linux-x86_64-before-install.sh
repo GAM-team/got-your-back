b2334bc32ae0ee77a76db89bba3e55f0b99ee4d4
Jay Lee <jay0lee@gmail.com>
2019-07-26 16:14:36 -0400

    curl over wget

--- travis/linux-x86_64-before-install.sh
+++ travis/linux-x86_64-before-install.sh
@@ -40,7 +40,7 @@ cd ~/pybuild
 
 # Compile latest Python
 if [ ! -d Python-$BUILD_PYTHON_VERSION ]; then
-  wget --quiet https://www.python.org/ftp/python/$BUILD_PYTHON_VERSION/Python-$BUILD_PYTHON_VERSION.tar.xz
+  curl -O https://www.python.org/ftp/python/$BUILD_PYTHON_VERSION/Python-$BUILD_PYTHON_VERSION.tar.xz
   echo "Extracting Python..."
   tar xf Python-$BUILD_PYTHON_VERSION.tar.xz
 fi
