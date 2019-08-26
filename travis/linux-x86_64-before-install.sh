4deadd23b4318c6285d42e188b2ea2bc99af891f
Jay Lee <jay0lee@gmail.com>
2019-08-26 10:58:32 -0400

    more travis fixes

--- travis/linux-x86_64-before-install.sh
+++ travis/linux-x86_64-before-install.sh
@@ -76,13 +76,29 @@ else
 
   $python -V
 
-  if [[ "$dist" == "xenial" ]]; then
+  if [[ "$dist" == "precise" ]]; then
     echo "Installing deps for StaticX..."
-    sudo apt-get install --yes patchelf scons musl
+    sudo apt-get install --yes scons
+    if [ ! -d patchelf-$PATCHELF_VERSION ]; then
+      echo "Downloading PatchELF $PATCHELF_VERSION"
+      wget https://nixos.org/releases/patchelf/patchelf-$PATCHELF_VERSION/patchelf-$PATCHELF_VERSION.tar.bz2
+      tar xf patchelf-$PATCHELF_VERSION.tar.bz2
+      cd patchelf-$PATCHELF_VERSION
+      ./configure
+      make
+      sudo make install
+    fi
+    if [ ! -d musl=$MUSL_VERSION ]; then
+      echo "Downloading MUSL $MUSL_VERSION"
+      wget https://www.musl-libc.org/releases/musl-$MUSL_VERSION.tar.gz
+      tar xf musl-$MUSL_VERSION.tar.gz
+      cd musl-$MUSL_VERSION
+      ./configure
+      make
+      sudo make install
+    fi
     $pip install git+https://github.com/JonathonReinhart/staticx.git@master
   fi
-
-  cd $whereibelong
 
   echo "Upgrading pip packages..."
   $pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 $pip install -U
