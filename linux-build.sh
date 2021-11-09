42fe82631265255a11f40766959f74aa2349df76
Jay Lee <jay0lee@gmail.com>
2021-11-09 10:19:57 -0500

    Delete linux-build.sh

--- linux-build.sh
+++
@@ -1,9 +0,0 @@
-rm -rf gyb
-rm -rf build
-rm -rf dist
-rm -rf gyb-$1-linux-$(arch).tar.xz
-
-export LD_LIBRARY_PATH=/usr/local/lib
-python3.7 /usr/local/bin/pyinstaller -F --clean --distpath=gyb linux-gyb.spec
-cp LICENSE gyb/
-tar cJf gyb-$1-linux-$(arch).tar.xz gyb/
