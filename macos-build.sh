196693bad3d0c126abeb8cf9bda4ecb3ddae01e1
Jay Lee <jay0lee@gmail.com>
2021-11-09 10:20:17 -0500

    Delete macos-build.sh

--- macos-build.sh
+++
@@ -1,8 +0,0 @@
-rm -rf gyb
-rm -rf build
-rm -rf dist
-rm -rf gyb-$1-macos.tar.xz
-
-/Library/Frameworks/Python.framework/Versions/3.7/bin/pyinstaller -F --clean --distpath=gyb macos-gyb.spec
-cp LICENSE gyb/
-tar cJf gyb-$1-macos.tar.xz gyb/
